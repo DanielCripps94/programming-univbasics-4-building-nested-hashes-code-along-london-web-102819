197bab79d1a2002657156910bbb739dfc64929ad
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-24 14:10:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,13 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriach =>
+      :matriach =>
+      :hero => {}
+      :her_friends =>
       }, 
     :capulet => {
+      
       #your key/value pairs here!
       }}
 
